0ba1796ee99d764bf539d16d65451a5b1e44340b
JRAdams97 <justinadams7991@gmail.com>
2020-01-19 00:05:52 +1100

    feat(setup): Setup project using Spring Initializer
    - add initializer content for Springtune

---
+++ src/main/java/au/com/jradams/springtune/SpringtuneApplication.java
@@ -0,0 +1,13 @@
+package au.com.jradams.springtune;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtuneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtuneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/au/com/jradams/springtune/SpringtuneApplicationTests.java
@@ -0,0 +1,13 @@
+package au.com.jradams.springtune;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtuneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
