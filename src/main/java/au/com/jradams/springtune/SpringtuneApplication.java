46fa8e412113b59f78d531eed8f6b8833b0a1e0e
JRAdams97 <justinadams7991@gmail.com>
2020-02-09 14:36:15 +1100

    feat(setup): Implement basic JSP handling
    - add auto config annotation to application context
    - add basic index controller
    - add basic index JSP page
    - add JSP view resolver properties
    - remove ServletInitializer and merge with application context

--- src/main/java/au/com/jradams/springtune/SpringtuneApplication.java
+++ src/main/java/au/com/jradams/springtune/SpringtuneApplication.java
@@ -1,13 +1,21 @@
 package au.com.jradams.springtune;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@EnableAutoConfiguration
 @SpringBootApplication
-public class SpringtuneApplication {
+public class SpringtuneApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringtuneApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringtuneApplication.class, args);
 	}
-
 }
